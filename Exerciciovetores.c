9d2dd8b44ec1cf0e92e677041434b1f8fe1d0d4d
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2022-10-12 15:22:01 -0300

    new codes

---
+++ Exerciciovetores.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+int tamanho, maior, menor;
+
+printf("Digite o tamanho do vetor: \n");
+scanf("%d", &tamanho);
+
+int vetor[tamanho];
+
+for (int i = 0; i < tamanho; i++){
+
+printf("Digite o numero da posicao [%d] do vetor: \n", i);
+scanf("%d", &vetor[i]);
+
+}
+
+menor = vetor[0];
+maior = vetor[0];
+
+for(int i = 1; i < tamanho; i++) {
+
+    if(vetor[i] > maior){
+
+        maior = vetor[i];
+    }
+
+    if(vetor[i] < menor){
+
+        menor = vetor[i];
+    }
+
+}
+
+printf("Maior valor: %d\n", maior);
+printf("Menor valor: %d\n", menor);
+
+
+}
