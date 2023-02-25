df69faac63c753dafd4129ae057857f2d9fabb60
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2023-02-25 13:16:14 -0300

    logica

--- tiposEstruturados/Atividade/práticamatriz/Quest1.c
+++ tiposEstruturados/Atividade/práticamatriz/Quest1.c
@@ -55,40 +55,40 @@ int main(){
             {     
                 
                 
-                // se for o canto superior esquerdo
+                
                 if ((i == 0 && j == 0 && (i + 1 < m && Matriz[i + 1][j] == 42) || (j + 1 < n && Matriz[i][j + 1] == 42))) {
                     total++;
                 } 
-                // se for o canto superior direito
+               
                 else if ((i == 0 && j == n - 1 && (i + 1 < m && Matriz[i + 1][j] == 42) || (j > 0 && Matriz[i][j - 1] == 42))) {
                     total++;
                 } 
-                // se for o canto inferior esquerdo
+                
                 else if ((i == m - 1 && j == 0 && (i > 0 && Matriz[i - 1][j] == 42) || (j + 1 < n && Matriz[i][j + 1] == 42))) {
                     total++;
                 } 
-                // se for o canto inferior direito
+                
                 else if ((i == m - 1 && j == n - 1 && (i > 0 && Matriz[i - 1][j] == 42) || (j > 0 && Matriz[i][j - 1] == 42))) {
                     total++;
                       
                 } 
-                // se for a primeira linha
+                
                 else if ((i == m - 1 && j > 0 && j < n - 1 && (Matriz[i][j - 1] == 42 || Matriz[i][j + 1] == 42))) {
                     total++;
                 } 
-                // se for a ultima linha
+               
                 else if ((j == n - 1 && i > 0 && i < m - 1 && (Matriz[i - 1][j] == 42 || Matriz[i + 1][j] == 42))) {
                     total++;
                 }
-                // se for a primeira coluna
+                
                  else if ((i == 0 && j > 0 && j < n - 1 && (Matriz[i][j - 1] == 42 || Matriz[i][j + 1] == 42))) {
                     total++;
                 }
-                // se for a ultima coluna
+                
                  else if ((j == 0 && i > 0 && i < m - 1 && (Matriz[i - 1][j] == 42 || Matriz[i + 1][j] == 42))) {
                     total++;
                 } 
-                // se for o meio
+                
                 else if ((i != 0 && j != 0 && j != n - 1 && i != m - 1) && (Matriz[i - 1][j] == 42 || Matriz[i + 1][j] == 42 || Matriz[i][j - 1] == 42 || Matriz[i][j + 1] == 42)) {
                     total++;
                 }
@@ -107,3 +107,15 @@ int main(){
 
 
 }
+
+/*
+  
+  A logica utilizada foi criar uma matriz com os numeros corespondentes aos caracteres na tabela ascii
+  e para gerar aleatoriamente o mapa criei um vetor com esses dois numeros junto com uma variavel indice
+  indice vai receber um numero aleatorio entre 1 e 0, e a posição da matriz vai receber um valor do vetor numero
+  no indice da variavel indice, como a variavel indice recebe um valor aleatorio entre 1 e 0, a matriz vai receber valores
+  aleatorios variando de 35 e 42, sendo 35 = # e 42 = *. com a matriz preenchida tive um pouco de dificuldade para realizar a lógica
+  de verifição porém consiste apenas em verificar os 4 lados de cada posição da matriz, tomando cuidado para nao exceder
+  o numero de linhas e de colunas durante a verificaçao
+
+*/
