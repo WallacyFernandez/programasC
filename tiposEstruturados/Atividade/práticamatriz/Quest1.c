800f71bde19c4fb2afb2b10e8604314a4435fef1
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2023-02-24 23:20:23 -0300

    new past

---
+++ tiposEstruturados/Atividade/práticamatriz/Quest1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char **Matriz = (int **)malloc(5 * sizeof(int *));
+    int i, j;
+
+    for (i = 0; i < 5; i++)
+    {
+
+        Matriz[i] = (int *)malloc(5 * sizeof(int));
+
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5; j++){
+
+            printf("Preencha a matriz com ")
+        }
+
+    }
+
+
+
+
+}
